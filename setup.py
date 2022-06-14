4367d359c2989007ef4fbfed207cdb860fd9c82f
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-06-14 06:23:54 -0400

    .

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 from os import PathLike
 from typing import Union, TypeAlias
+from getpass import getuser
 
 class Types:
     StrOrBytesPath: TypeAlias = str | bytes | PathLike[str] | PathLike[bytes]
@@ -8,19 +9,28 @@ class Types:
 
 class Install:
     def __init__(self) -> None:
-        pass
+        self.__HOME_FOLDER = os.path.join('/home', str(getuser()))
+        self.__WORK_FOLDER = os.path.join(self.__HOME_FOLDER, 'setup')
     
-    def _change_dir(path: Types.FdOrAnyPath = ""):
+    def _change_dir(self, path: Types.FdOrAnyPath = None):
+        if path is None:
+            path = self.__WORK_FOLDER
         try:
             if not(os.getcwd() == path):
-                os.chdir(path)
+                try:
+                    os.chdir(path)
+                except FileNotFoundError:
+                    os.makedirs(path)
+                    os.chdir(path)
             return True
         except:
             return False
     
     def yay(self):
         pass
+    
 
 x = Install()
-x._change_dir()
-print(os.getcwd())
+y = x._change_dir()
+
+print(os.getcwd(), y)
