5c6ffb0db18223eaddb5404108810b85b0b92cbb
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-06-19 17:09:16 +0530

    .

--- setup.py
+++ setup.py
@@ -11,6 +11,51 @@ class Install:
     def __init__(self) -> None:
         self.__HOME_FOLDER = os.path.join('/home', str(getuser()))
         self.__WORK_FOLDER = os.path.join(self.__HOME_FOLDER, 'setup')
+        self.__install_list = [
+            "brave-bin", 
+            "spotify",
+            "github-desktop-bin",
+            "bitwarden",
+            "firefox",
+            "visual-studio-code-bin",
+            "ffmpegthumbs",
+            "mplayerthumbs",
+            "qbittorrent",
+            "teamviewer",
+            "anydesk",
+            "mongodb-compass",
+            "virtualbox",
+            "rawtherapee",
+            "telegram-desktop-bin",
+            "kdenlive",
+            "openshot",
+            "audacity",
+            "handbrake",
+            "stacer",
+            "eclipse",
+            "jdk8-openjdk",
+            "pycharm-community-edition",
+            "kdeconnect",
+            "python",
+            "python-pip",
+            "python2",
+            "nodejs",
+            "npm",
+            "tcl",
+            "fpc",
+            "kompozer",
+            "spotiflyer-bin",
+            "youtube-dl",
+            "etcher",
+            "shotcut",
+            "notepadqq",
+            "yakuake",
+            "sonixd-appimage",
+            "flatpak",
+            "pinta",
+            "photopea"
+        ]   
+    
     
     def _change_dir(self, path: Types.FdOrAnyPath = None):
         if path is None:
@@ -26,18 +71,23 @@ class Install:
         except:
             return False
     
-    def yay(self):
+    def _install_yay(self):
+        self._change_dir()
         os.system("git clone https://aur.archlinux.org/yay.git")
         os.chdir(os.path.join(os.getcwd(), "yay"))
         os.system("makepkg -si")
         os.chdir("..")
         os.remove(os.path.join(os.getcwd(), "yay"))
     
-    def others(self):
-        pass    
+    def packages(self):
+        self._install_yay()
+        os.system(
+            "yay -Syy  {packages}".format(
+                packages=" ".join(self.__install_list)
+            )
+        )
+
 
 x = Install()
-x._change_dir()
-x.bitwarden()
 
 print(os.getcwd())
